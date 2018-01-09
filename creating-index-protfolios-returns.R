1e6b0a7216e9f2484b7ff68ba4cd432a30a90613
Sebastian Bucki <sebastian.bucki@gmail.com>
2018-01-09 13:17:23 +0100

    Consider lack of data for given companies in stooq.com

--- creating-index-protfolios-returns.R
+++ creating-index-protfolios-returns.R
@@ -29,6 +29,31 @@ for(ticker in tickers){
                                                   sep=""),
                   destfile = paste("./companies_and_index_historical_data/", 
                                    ticker,".csv", sep = ""))
+    }
+}
+
+# Transform downladed data
+
+# Update files
+exstg_files <- unlist(lapply(list.files("./companies_and_index_historical_data"),
+                             gsub, pattern = ".csv", replacement = ""))
+file_list <- list()
+no_data_files <- character()
+for(i in 1:length(exstg_files)){
+  file_list[[i]] <- read.csv(paste("./companies_and_index_historical_data/", 
+                                    exstg_files[i], ".csv", sep=""), header=TRUE)
+  # Consider lack of data
+  if(colnames(file_list[[i]])[1] != "No.data"){
+  file_list[[i]] <- xts(file_list[[i]][c("Close")], 
+                          order.by = as.Date(file_list[[i]]$Date))
+  file_list[[i]] <-diff(log(file_list[[i]]))
+  names(file_list[[i]]) <- c(paste(exstg_files[i], "rr", sep = "_"))
+  }else{
+    no_data_files <- append(exstg_files[i], no_data_files)
   }
 }
+
+# No data files print and save as csv
+print(paste("No data for:", no_data_files))
+write.csv(no_data_files, "stooq_no_data_files.csv")
 
